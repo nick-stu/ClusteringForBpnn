b04cb5928dbc7aac9ba90d8ba0ea2ffa44e1c999
nick-stu <1733763428@qq.com>
2018-05-30 09:19:02 +0800

    updae

--- getLabelsNew.m
+++ getLabelsNew.m
@@ -6,22 +6,7 @@ function[trainLabel,tag]=getLabelsNew(trainData)
     tag=[]; trainLabel=[];
     cursor=0;
     for i = 1:9
-        mdl.label=[ones(1,20)*1  ones(1,20)*2 ones(1,20)*3 ones(1,20)*4 ones(1,20)*5];
-        %% 破坏人工性
-%         index=randperm(length(mdl.label),5);
-%         mdl.label(index)=1;
-%         index=randperm(length(mdl.label),5);
-%         mdl.label(index)=2;
-%         index=randperm(length(mdl.label),5);
-%         mdl.label(index)=3;
-%         index=randperm(length(mdl.label),5);
-%         mdl.label(index)=4;
-%         index=randperm(length(mdl.label),5);
-%         mdl.label(index)=5;
-%         % 查看破坏度
-%         D = length(mdl.label) - length( find(mdl.label-[ones(1,20)*1  ones(1,20)*2 ones(1,20)*3 ones(1,20)*4 ones(1,20)*5] == 0) );
-%         fprintf("---%d\n",D);
-        %%
+        mdl.label=[ones(1,singleNum/5)*1 ones(1,singleNum/5)*2 ones(1,singleNum/5)*3 ones(1,singleNum/5)*4 ones(1,singleNum/5)*5];
         label=zeros(singleNum,rows);
         for j=1:singleNum
             label(j,mdl.label(j)+cursor)=1; 
@@ -32,16 +17,14 @@ function[trainLabel,tag]=getLabelsNew(trainData)
     end
     trainLabel(:, find( sum(trainLabel,1)==0) )=[];
     fprintf('classNum:%d\t',size(tag,2));
-%%
+%% 聚类
 %     rows = size(trainData, 1);
 %     singleNum = rows / 9;
 %     tag=[]; trainLabel=[];
 %     cursor=0;
 %     for i = 1:9
-% %         mdl=clustering_dis_offset( trainData( (i-1)*singleNum+1 : i*singleNum,:),false );
-% %         mdl=clustering( trainData( (i-1)*singleNum+1 : i*singleNum,:) ,false);
-% %         mdl=k_means(trainData( (i-1)*singleNum+1 : i*singleNum,:),5);
 %         mdl=clustering_offset_nearest( trainData( (i-1)*singleNum+1 : i*singleNum,:),false );
+% %         mdl=clustering_offset_nearest_MhtD( trainData( (i-1)*singleNum+1 : i*singleNum,:),false );
 %         label=zeros(singleNum,rows);
 %         for j=1:singleNum
 %             label(j,mdl.label(j)+cursor)=1; 
@@ -49,6 +32,11 @@ function[trainLabel,tag]=getLabelsNew(trainData)
 %         cursor=cursor+mdl.classNum;
 %         tag=[tag repmat(i,[1,mdl.classNum])];
 %         trainLabel=[trainLabel;label];
+%         
+%         %% show detail
+%         for z=1:mdl.classNum
+%            fprintf('%d\n',length(find(mdl.label==z))); 
+%         end
 %     end
 %     trainLabel(:, find( sum(trainLabel,1)==0) )=[];
 %     fprintf('classNum:%d\t',size(tag,2));
