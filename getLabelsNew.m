a96becac3c318ae01b07a67e4f5f21c994e353c4
nick-stu <1733763428@qq.com>
2018-06-02 11:42:38 +0800

    update GetLabelsNew.m ,join the judgement

--- getLabelsNew.m
+++ getLabelsNew.m
@@ -1,46 +1,49 @@
-function[trainLabel,tag]=getLabelsNew(trainData)
+function[trainLabel,tag]=getLabelsNew(trainData,isClu)
 %% 注意这里引用的聚类函数名称记得改
-%% 人工分类
-    fprintf('----manual sorting----\n');
-    rows = size(trainData, 1);
-    singleNum = rows / 9;
-    tag=[]; trainLabel=[];
-    cursor=0;
-    for i = 1:9
-        mdl.label=[ones(1,singleNum/5)*1 ones(1,singleNum/5)*2 ones(1,singleNum/5)*3 ones(1,singleNum/5)*4 ones(1,singleNum/5)*5];
-        label=zeros(singleNum,rows);
-        for j=1:singleNum
-            label(j,mdl.label(j)+cursor)=1; 
+    if isClu==false
+        %% 人工分类
+        fprintf('----manual sorting----\n');
+        rows = size(trainData, 1);
+        singleNum = rows / 9;
+        tag=[]; trainLabel=[];
+        cursor=0;
+        for i = 1:9
+            mdl.label=[ones(1,singleNum/5)*1 ones(1,singleNum/5)*2 ones(1,singleNum/5)*3 ones(1,singleNum/5)*4 ones(1,singleNum/5)*5];
+            label=zeros(singleNum,rows);
+            for j=1:singleNum
+                label(j,mdl.label(j)+cursor)=1; 
+            end
+            cursor=cursor+5;
+            tag=[tag repmat(i,[1,5])];
+            trainLabel=[trainLabel;label];
         end
-        cursor=cursor+5;
-        tag=[tag repmat(i,[1,5])];
-        trainLabel=[trainLabel;label];
+        trainLabel(:, find( sum(trainLabel,1)==0) )=[];
+        fprintf('classNum:%d\t',size(tag,2));
+    else
+        %% 聚类
+        fprintf('----clustering----\n');
+        rows = size(trainData, 1);
+        singleNum = rows / 9;
+        tag=[]; trainLabel=[];
+        cursor=0;
+        for i = 1:9
+            mdl=clustering_offset_nearest( trainData( (i-1)*singleNum+1 : i*singleNum,:),false );
+    %         mdl=clustering_offset_nearest_MhtD( trainData( (i-1)*singleNum+1 : i*singleNum,:),false );
+            label=zeros(singleNum,rows);
+            for j=1:singleNum
+                label(j,mdl.label(j)+cursor)=1; 
+            end
+            cursor=cursor+mdl.classNum;
+            tag=[tag repmat(i,[1,mdl.classNum])];
+            trainLabel=[trainLabel;label];
+
+            %% show detail
+            for z=1:mdl.classNum
+               fprintf('%d ',length(find(mdl.label==z))); 
+            end
+            fprintf('\n');
+        end
+        trainLabel(:, find( sum(trainLabel,1)==0) )=[];
+        fprintf('classNum:%d\t',size(tag,2));
     end
-    trainLabel(:, find( sum(trainLabel,1)==0) )=[];
-    fprintf('classNum:%d\t',size(tag,2));
-%% 聚类
-%     fprintf('----clustering----\n');
-%     rows = size(trainData, 1);
-%     singleNum = rows / 9;
-%     tag=[]; trainLabel=[];
-%     cursor=0;
-%     for i = 1:9
-%         mdl=clustering_offset_nearest( trainData( (i-1)*singleNum+1 : i*singleNum,:),false );
-% %         mdl=clustering_offset_nearest_MhtD( trainData( (i-1)*singleNum+1 : i*singleNum,:),false );
-%         label=zeros(singleNum,rows);
-%         for j=1:singleNum
-%             label(j,mdl.label(j)+cursor)=1; 
-%         end
-%         cursor=cursor+mdl.classNum;
-%         tag=[tag repmat(i,[1,mdl.classNum])];
-%         trainLabel=[trainLabel;label];
-%         
-%         %% show detail
-%         for z=1:mdl.classNum
-%            fprintf('%d ',length(find(mdl.label==z))); 
-%         end
-%         fprintf('\n');
-%     end
-%     trainLabel(:, find( sum(trainLabel,1)==0) )=[];
-%     fprintf('classNum:%d\t',size(tag,2));
 end
