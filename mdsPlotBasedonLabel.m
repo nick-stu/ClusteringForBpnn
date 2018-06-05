b7b8133bd5c9b1a02f9d3f111aba9d7f864f6990
nick-stu <1733763428@qq.com>
2018-06-06 00:22:41 +0800

    Add two dimensional functions to two dimensional drawing functions

--- mdsPlotBasedonLabel.m
+++ mdsPlotBasedonLabel.m
@@ -1,12 +1,23 @@
 function[]=mdsPlotBasedonLabel(data,K,label,Pointsize)
 data=MDS(data,K);
 figure;
-for i=1:size(label,2)
-    index=find(label==i);
-    a=rand; b=rand; c=rand;
-    for j=1:length(index)
-        scatter3(data(index(j),1),data(index(j),2),data(index(j),3), Pointsize,[a b c],'filled');hold on;
-        text(data(index(j),1),data(index(j),2),data(index(j),3),num2str(label(index(j))));
+if K==3
+    for i=1:size(label,2)
+        index=find(label==i);
+        a=rand; b=rand; c=rand;
+        for j=1:length(index)
+            scatter3(data(index(j),1),data(index(j),2),data(index(j),3), Pointsize,[a b c],'filled');hold on;
+            text(data(index(j),1),data(index(j),2),data(index(j),3),num2str(label(index(j))));
+        end
+    end
+elseif K==2
+    for i=1:size(label,2)
+        index=find(label==i);
+        a=rand; b=rand; c=rand;
+        for j=1:length(index)
+            scatter(data(index(j),1),data(index(j),2), Pointsize,[a b c],'filled');hold on;
+            text(data(index(j),1),data(index(j),2),num2str(label(index(j))));
+        end
     end
 end
 title('MDS based on label');
--- pcaPlotBasedonLabel.m
+++ pcaPlotBasedonLabel.m
@@ -1,5 +1,5 @@
 function [data] = pcaPlotBasedonLabel(data,K,label,Pointsize)
-data=featureNormalize(data);
+[data,~]=featureNormalize(data,[1]);
 [eigenVectors,scores,eigenValues] = pca(data);
 transMatrix = eigenVectors(:,1:K);
 data = data*transMatrix;
@@ -7,12 +7,23 @@ fprintf('∫œ¿Ì∂»£∫%f\n',sum(eigenValues(1:K))/sum(eigenValues));
 
 
 figure;
-for i=1:size(label,2)
-    index=find(label==i);
-    a=rand; b=rand; c=rand;
-    for j=1:length(index)
-        scatter3(data(index(j),1),data(index(j),2),data(index(j),3), Pointsize,[a b c],'filled');hold on;
-        text(data(index(j),1),data(index(j),2),data(index(j),3),num2str(label(index(j))));
+if K==3
+    for i=1:size(label,2)
+        index=find(label==i);
+        a=rand; b=rand; c=rand;
+        for j=1:length(index)
+            scatter3(data(index(j),1),data(index(j),2),data(index(j),3), Pointsize,[a b c],'filled');hold on;
+            text(data(index(j),1),data(index(j),2),data(index(j),3),num2str(label(index(j))));
+        end
+    end
+elseif K==2
+    for i=1:size(label,2)
+        index=find(label==i);
+        a=rand; b=rand; c=rand;
+        for j=1:length(index)
+            scatter(data(index(j),1),data(index(j),2), Pointsize,[a b c],'filled');hold on;
+            text(data(index(j),1),data(index(j),2),num2str(label(index(j))));
+        end
     end
 end
 title('PCA based on label');
