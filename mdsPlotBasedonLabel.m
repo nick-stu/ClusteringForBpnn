c9ff24b21c89235458fd4ca3e914899a032c6751
nick-stu <1733763428@qq.com>
2018-06-06 16:52:24 +0800

    set more visible color about text

--- mdsPlotBasedonLabel.m
+++ mdsPlotBasedonLabel.m
@@ -4,19 +4,19 @@ figure;
 if K==3
     for i=1:size(label,2)
         index=find(label==i);
-        a=rand; b=rand; c=rand;
+        a=rand*0.6+0.4; b=rand*0.6+0.4; c=rand*0.6+0.4;
         for j=1:length(index)
             scatter3(data(index(j),1),data(index(j),2),data(index(j),3), Pointsize,[a b c],'filled');hold on;
-            text(data(index(j),1),data(index(j),2),data(index(j),3),num2str(label(index(j))), 'Color',[1-a,1-b,1-c]);
+            text(data(index(j),1),data(index(j),2),data(index(j),3),num2str(label(index(j))));
         end
     end
 elseif K==2
     for i=1:size(label,2)
         index=find(label==i);
-        a=rand; b=rand; c=rand;
+        a=rand*0.6+0.4; b=rand*0.6+0.4; c=rand*0.6+0.4;
         for j=1:length(index)
             scatter(data(index(j),1),data(index(j),2), Pointsize,[a b c],'filled');hold on;
-            text(data(index(j),1),data(index(j),2),num2str(label(index(j))), 'Color',[1-a,1-b,1-c]);
+            text(data(index(j),1),data(index(j),2),num2str(label(index(j))));
         end
     end
 end
