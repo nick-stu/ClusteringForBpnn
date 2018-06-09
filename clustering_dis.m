2c729f92e405bdf13025fe83e44cd8fcc5c9d457
nick-stu <1733763428@qq.com>
2018-06-09 21:51:32 +0800

    Include the called function in this file

--- clustering_dis.m
+++ clustering_dis.m
@@ -10,7 +10,7 @@ function [ mdl ] = clustering_dis( data )
     dis = zeros(1, num);
     p_label = 1:num; % data标志序列
     [avg,std]=classStatus(data);
-    threshold = avg-std; % 这里是平均距离
+    threshold = (avg-std)*1.05; % 这里是平均距离
 %     fprintf('threshold: %s ',threshold);
     %% 找出两两距离最小的两个点
     D = pdist2(data, data);
@@ -146,4 +146,19 @@ function [ mdl ] = clustering_dis( data )
     
     mdl.label = label;
     mdl.classNum = classNum;
+end
+
+function[distance,avg]=distanceChange(sample,traindata)
+    [num,~]=size(traindata);
+    total=pdist2(sample,traindata);
+    distance=sum(total(:))/ num;
+    if num==1   % 如果已有类只有一个点，那选取最近的点加入就行了，不用判断阈值
+        avg=0;
+    else
+%         data=[traindata;sample]; % 这里是平均距离
+%         [num,~]=size(data);
+%         total=pdist2(data,data); 
+%         avg = sum(total(:))/(num*(num-1));
+        avg=distance; % 这里是点与类的平均距离
+    end
 end
