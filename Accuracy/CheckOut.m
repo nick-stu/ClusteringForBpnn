75df2e56ecbc789ddeec098f19206daba83d3223
nick-stu <1733763428@qq.com>
2018-07-08 19:03:01 +0800

    upload

---
+++ Accuracy/CheckOut.m
@@ -0,0 +1,16 @@
+close all;
+figure;plot(A');title('A');grid on;
+figure;plot(B');title('B');grid on;
+profit=mean(B(:))-mean(A(:));
+
+figure;
+subplot(3,1,1);plot(mean(B)-mean(A),'k');title(num2str(profit));
+hold on;grid on;
+plot([1 size(A,2)],[0 0],'r');
+plot([1 size(A,2)],[0.01 0.01],'r');
+plot([1 size(A,2)],[-0.01 -0.01],'r');
+
+subplot(3,1,2);plot(mean(A),'k');title(num2str(mean( A(:) ) ));
+hold on;grid on;plot([1 size(A,2)],[mean(A(:)) mean(A(:))],'r');
+subplot(3,1,3);plot(mean(B),'k');title(num2str(mean( B(:) ) ));
+hold on;grid on;plot([1 size(B,2)],[mean(B(:)) mean(B(:))],'r');
