b2b6b5e0863f470b37ab3c33fc8f6d4efcdec6ec
nick-stu <1733763428@qq.com>
2018-07-18 14:47:49 +0800

    call the SubstituteAlgo

--- KNN/CalAccuracy_KNN_withUpdate.m
+++ KNN/CalAccuracy_KNN_withUpdate.m
@@ -6,8 +6,11 @@ function [accuracy]= CalAccuracy_KNN_withUpdate(trainData, testData, trainLabel,
         if result == testLabel(i)
             trueNum = trueNum + 1;
         else
-            trainData(chosenOne,:)=testData(i,:); % 删去误导性样本,用最新的进行替代
-            trainLabel(chosenOne)=testLabel(i);
+%             trainData(chosenOne,:)=testData(i,:); % 删去误导性训练样本,用判断错误的测试样本进行替代
+%             trainLabel(chosenOne)=testLabel(i);
+%             trainData=[trainData ;testData(i,:)]; % 不删去误导性训练样本,加入判断错误的测试样本
+%             trainLabel=[trainLabel; testLabel(i)];
+            [trainData,trainLabel] = SubstituteAlgo(testData(i,:),trainData,trainLabel,testLabel(i),chosenOne);
         end
     end
     accuracy = trueNum / rows;
