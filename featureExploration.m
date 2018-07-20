0a96f1538094ae1f469ea403b728edffd0a3720e
nick-stu <1733763428@qq.com>
2018-07-20 08:59:21 +0800

    update

--- featureExploration.m
+++ featureExploration.m
@@ -1,7 +1,7 @@
 function [inner,diff,threshold,avg]=featureExploration(data)
-[A,std]=classStatus(data);
-threshold=A ;
-% threshold=threshold*1.4;
+[A,std]=classStatus_MhtD(data);
+threshold=A - std;
+threshold=threshold*1.15;
 %% Split data
 over=data(1:30,:);
 below=data(31:60,:);
@@ -9,37 +9,37 @@ left=data(61:90,:);
 right=data(91:120,:);
 center=data(121:150,:);
 %% cumulate
-for i=1:30
-    [tmp,~]=classStatus(below(1:i,:));
-    fprintf('%.4f  %.4f\n',tmp,threshold);
-end
-fprintf('---------------------\n');
-for i=1:30
-    [tmp,~]=classStatus([below ;center(1:i,:) ]);
-    fprintf('%.4f  %.4f\n',tmp,threshold);
-end
+% for i=1:30
+%     [tmp,~]=classStatus_MhtD(left(1:i,:));
+%     fprintf('%.4f  %.4f\n',tmp,threshold);
+% end
+% fprintf('-----------------------\n');
+% for i=1:30
+%     [tmp,~]=classStatus_MhtD([left ;center(1:i,:)]);
+%     fprintf('%.4f  %.4f\n',tmp,threshold);
+% end
 %% each type 
 inner=[];
-[avg,~]=classStatus(over); inner=[inner avg];
-[avg,~]=classStatus(below); inner=[inner avg];
-[avg,~]=classStatus(left); inner=[inner avg];
-[avg,~]=classStatus(right); inner=[inner avg];
-[avg,~]=classStatus(center); inner=[inner avg];
+[avg,~]=classStatus_MhtD(over); inner=[inner avg];
+[avg,~]=classStatus_MhtD(below); inner=[inner avg];
+[avg,~]=classStatus_MhtD(left); inner=[inner avg];
+[avg,~]=classStatus_MhtD(right); inner=[inner avg];
+[avg,~]=classStatus_MhtD(center); inner=[inner avg];
 %% merged types 
 diff=[];
-[avg,~]=classStatus([over below]); diff=[diff avg];
-[avg,~]=classStatus([left right]); diff=[diff avg];
 
-[avg,~]=classStatus([below left]); diff=[diff avg];
-[avg,~]=classStatus([below right]); diff=[diff avg];
-[avg,~]=classStatus([over left]); diff=[diff avg];
-[avg,~]=classStatus([over right]); diff=[diff avg];
+[avg,~]=classStatus_MhtD([over; center]); diff=[diff avg];
+[avg,~]=classStatus_MhtD([left; center]); diff=[diff avg];
+[avg,~]=classStatus_MhtD([below; center]); diff=[diff avg];
+[avg,~]=classStatus_MhtD([right; center]); diff=[diff avg];
 
-[avg,~]=classStatus([over center]); diff=[diff avg];
-[avg,~]=classStatus([left center]); diff=[diff avg];
-[avg,~]=classStatus([below center]); diff=[diff avg];
-[avg,~]=classStatus([right center]); diff=[diff avg];
+[avg,~]=classStatus_MhtD([below; left]); diff=[diff avg];
+[avg,~]=classStatus_MhtD([below; right]); diff=[diff avg];
+[avg,~]=classStatus_MhtD([over; left]); diff=[diff avg];
+[avg,~]=classStatus_MhtD([over; right]); diff=[diff avg];
 
+[avg,~]=classStatus_MhtD([over; below]); diff=[diff avg];
+[avg,~]=classStatus_MhtD([left; right]); diff=[diff avg];
 %% plot
 % figure;
 % ALL=[inner diff];
