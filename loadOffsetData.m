804979db47df346be24d71663de29531f8ffd398
nick-stu <1733763428@qq.com>
2018-07-20 09:16:35 +0800

    update

--- loadOffsetData.m
+++ loadOffsetData.m
@@ -79,65 +79,67 @@ if strcmp(mode,'old')
 elseif strcmp(mode,'new')
     %%
     basePath = './chj/data/20-300/';
-    begin=120*4 +1; tail=begin+119;
-    outlabel=[ones(1,120)*1 ones(1,120)*2 ones(1,120)*3 ones(1,120)*4 ones(1,120)*5];
-    DBIndex=[];
-    %% Load DATA
-    load([basePath, '/decimate_overdata20-300_69k_0.6k.mat']);
-    over = decimate_data(begin:tail, :);
-    load([basePath, '/decimate_belowdata20-300_69k_0.6k.mat']);
-    below = decimate_data(begin:tail, :);
-    load([basePath, '/decimate_leftdata20-300_69k_0.6k.mat']);
-    left = decimate_data(begin:tail, :);
-    load([basePath, '/decimate_rightdata20-300_69k_0.6k.mat']);
-    right = decimate_data(begin:tail, :);
-    load([basePath, '/decimate_centerdata20-300_69k_0.6k.mat']);
-    center = decimate_data(begin:tail, :);
-    %% GCC
-    partSize=size(over,1);
-    GCCData=BatchGCC_ExternalBase([over ;below ;left ;right ; center]);
-    over=GCCData(1:partSize,:); GCCData(1:partSize,:)=[];
-    below=GCCData(1:partSize,:); GCCData(1:partSize,:)=[];
-    left=GCCData(1:partSize,:); GCCData(1:partSize,:)=[];
-    right=GCCData(1:partSize,:); GCCData(1:partSize,:)=[];
-    center=GCCData(1:partSize,:); GCCData(1:partSize,:)=[];
-    %% PSD
-    psdData1 = PSD(over, 600);
-    psdData2 = PSD(below, 600);
-    psdData3 = PSD(left, 600);
-    psdData4 = PSD(right, 600);
-    psdData5 = PSD(center, 600);
+    for i=0:8
+        begin=120*4 +1; tail=begin+119;
+        outlabel=[ones(1,120)*1 ones(1,120)*2 ones(1,120)*3 ones(1,120)*4 ones(1,120)*5];
+        DBIndex=[];
+        %% Load DATA
+        load([basePath, '/decimate_overdata20-300_69k_0.6k.mat']);
+        over = decimate_data(begin:tail, :);
+        load([basePath, '/decimate_belowdata20-300_69k_0.6k.mat']);
+        below = decimate_data(begin:tail, :);
+        load([basePath, '/decimate_leftdata20-300_69k_0.6k.mat']);
+        left = decimate_data(begin:tail, :);
+        load([basePath, '/decimate_rightdata20-300_69k_0.6k.mat']);
+        right = decimate_data(begin:tail, :);
+        load([basePath, '/decimate_centerdata20-300_69k_0.6k.mat']);
+        center = decimate_data(begin:tail, :);
+        %% GCC
+        partSize=size(over,1);
+        GCCData=BatchGCC_ExternalBase([over ;below ;left ;right ; center]);
+        over=GCCData(1:partSize,:); GCCData(1:partSize,:)=[];
+        below=GCCData(1:partSize,:); GCCData(1:partSize,:)=[];
+        left=GCCData(1:partSize,:); GCCData(1:partSize,:)=[];
+        right=GCCData(1:partSize,:); GCCData(1:partSize,:)=[];
+        center=GCCData(1:partSize,:); GCCData(1:partSize,:)=[];
+        %% PSD
+        psdData1 = PSD(over, 600);
+        psdData2 = PSD(below, 600);
+        psdData3 = PSD(left, 600);
+        psdData4 = PSD(right, 600);
+        psdData5 = PSD(center, 600);
 
-    over = cat(2,over,psdData1);
-    below = cat(2,below,psdData2);
-    left = cat(2,left,psdData3);
-    right = cat(2,right,psdData4);
-    center = cat(2,center,psdData5);
+        over = cat(2,over,psdData1);
+        below = cat(2,below,psdData2);
+        left = cat(2,left,psdData3);
+        right = cat(2,right,psdData4);
+        center = cat(2,center,psdData5);
 
-    %%
-    data=[over;below;left;right;center];
-    %% 归一化
-    %     data=featureNormalize(data);% 归一化
-    %% 聚类
-    %     mdl=clustering_dis_offset(data,false);
-    %     mdl=clustering_offset_nearest(data,false);
-    %     mdl=clusteringExploration(data,false);
-    %     mdl=clusteringDebug(data,false);
-    %     mdl=DBSCAN(data);
-    %     mdl=k_means(data,5);
-    %     mdl=clustering(data,true);
-    %% 查看聚类的密度情况
-    %     clusteringDensity(mdl);
-    %     densitySituationBasedOnType(data);
-    %     batchDTW(data);
-    %% 查看 DB指数
-    %     dbi=DBI(mdl);
-    %     DBIndex=[DBIndex dbi];
-    %% 可视化
-    %     pcaPlot_offset(originData,3,mdl.label);
-    %     mdsPlot_offset(data,3,mdl.label);
-    mdsPlotBasedonLabel(data,2,outlabel,20);
-    %     pcaPlotBasedonLabel(originData,2,outlabel,100);
+        %%
+        data=[over;below;left;right;center];
+        %% 归一化
+        %     data=featureNormalize(data);% 归一化
+        %% 聚类
+        %     mdl=clustering_dis_offset(data,false);
+        %     mdl=clustering_offset_nearest(data,false);
+        %     mdl=clusteringExploration(data,false);
+        %     mdl=clusteringDebug(data,false);
+        %     mdl=DBSCAN(data);
+        %     mdl=k_means(data,5);
+        %     mdl=clustering(data,true);
+        %% 查看聚类的密度情况
+        %     clusteringDensity(mdl);
+        %     densitySituationBasedOnType(data);
+        %     batchDTW(data);
+        %% 查看 DB指数
+        %     dbi=DBI(mdl);
+        %     DBIndex=[DBIndex dbi];
+        %% 可视化
+        %     pcaPlot_offset(originData,3,mdl.label);
+        %     mdsPlot_offset(data,3,mdl.label);
+        mdsPlotBasedonLabel(data,2,outlabel,100);
+        %     pcaPlotBasedonLabel(originData,2,outlabel,100);
 
-    % mean(DBIndex(~isnan(DBIndex)))
+        % mean(DBIndex(~isnan(DBIndex)))
+    end
 end
