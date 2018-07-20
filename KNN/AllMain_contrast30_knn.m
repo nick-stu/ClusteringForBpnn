45966c025d2ebc6004e23956fc76db4640f7fee7
nick-stu <1733763428@qq.com>
2018-07-20 09:44:31 +0800

    update

--- KNN/AllMain_contrast30_knn.m
+++ KNN/AllMain_contrast30_knn.m
@@ -1,5 +1,5 @@
-function[oldMat,newMat]=AllMain_contrast30_knn()
-clear;
+function[oldMat,newMat]=AllMain_contrast30_knn(Tsize)
+% clear;
 warning off;
 addpath(genpath(pwd));
 mode = 'Contrast_KNN';
@@ -13,7 +13,6 @@ for x=1:size(dirs, 1)
     if (dirs(x).name(1) == '.')
         continue;
     end
-    fprintf('%s ', dirs(x).name);
     
     %% original data
     load([basePath, dirs(x).name, '/decimate_data_0.6k.mat']);
@@ -25,53 +24,31 @@ for x=1:size(dirs, 1)
     %% confusion data
     data = cat(2,data,psdData);
     %% split data
-    [trainData, testData] = Split(data);
+    [trainData, testData] = Split(data,Tsize);
     %% generate label
     trainLabel = [];
     testLabel =[];
     for i = 1:9
-        trainLabel = [trainLabel ;repmat(i,[20,1])];
-        testLabel = [testLabel ;repmat(i,[10,1])];
+        trainLabel = [trainLabel ;repmat(i,[size(trainData,1)/9,1])];
+        testLabel = [testLabel ;repmat(i,[size(testData,1)/9,1])];
     end
     %% random index
-    idx=randperm(length(testLabel));
-    testLabel = testLabel(idx);
-    testData = testData(idx,:);
+%     idx=randperm(length(testLabel));
+%     testLabel = testLabel(idx);
+%     testData = testData(idx,:);
     %% test
-    fprintf('old:');
     accuracy1 = CalAccuracy_KNN (trainData, testData, trainLabel, testLabel, k);
-%     fprintf('%.4f\n',accuracy1);
-    fprintf('new:');
-
-    %% Multiple map label
-%     tmp=testLabel;testLabel=[];
-%     for m=1:size(tmp,2)
-%         testLabel=[testLabel repmat(tmp(:,m),[1,Mul])];
-%     end
-    %% BatchReliefF
-%     [trainData,testData]=BatchReliefF(trainData(:,1:frontSize),testData(:,1:frontSize),45);
-    %% VarFilter
-%     [trainData,testData]=VarFilter(trainData(:,1:frontSize),testData(:,1:frontSize));
-%     [trainData,testData]=mypca(trainData(:,1:frontSize),testData(:,1:frontSize),fix(frontSize*0.9));
-    %% Pow
-%     plot(data');
-%     trainData(:,40:frontSize)=[];%trainData(:,40:frontSize)*0.01;
-%     testData(:,40:frontSize)=[];%testData(:,40:frontSize)*0.01;
-    %% RePsd
-%     psdData1 = PSD(trainData, 600);
-%     psdData2 = PSD(testData, 600);
-%     trainData = cat(2,trainData,psdData1);
-%     testData = cat(2,testData,psdData2);
-    %% Fisher
-%     score = FisherScore( trainData, 9, 1 );
-%     trainData=score.*trainData;
-%     testData=score.*testData;
-    %% rmOutcast
-%     index=rmOutcastBasedonZscore(trainData);
-    %% z-score
-%     [trainData,testData] = featureNormalize( trainData,testData );  
-    accuracy2 = CalAccuracy_KNN_withUpdate (trainData, testData, trainLabel, testLabel, k);
-%     fprintf('%.4f\n',accuracy2);
+   %% Fisher
+    score = calVariance( trainData, 9);
+    trainData=score.*trainData;
+    testData=score.*testData;
+   %% reliefFscore
+    score = reliefFscore( trainData, trainLabel, 5);
+    trainData=score.*trainData;
+    testData=score.*testData;
+    
+    accuracy2 = CalAccuracy_KNN (trainData, testData, trainLabel, testLabel, k);
+%     accuracy2 = CalAccuracy_KNN_withUpdate (trainData, testData, trainLabel, testLabel, k);
     
     oldMat=[oldMat accuracy1];
     newMat=[newMat accuracy2];
